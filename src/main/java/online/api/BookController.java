c7c697b413f3bb19bfc5523b6892d57d009839ef
User <user@its.me>
2023-02-28 11:45:42 +0100

    api creation by spring

---
+++ src/main/java/online/api/BookController.java
@@ -0,0 +1,27 @@
+package online.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/api/books")
+    @ResponseBody
+    List<Book> getAllBooks() {
+        return List.of(
+            new Book("Harry Potter", "J.K. Rowling", 1997, 223),
+            new Book("Cień wiatru", "Carlos Ruiz Zafon", 2001, 565),
+            new Book("Latarnik", "Henryk Sienkiewicz", 1881, 24)
+        );
+    }
+    
+    @GetMapping("/api/books/1")
+    @ResponseBody
+    Book getSingleBook() {
+        return new Book("Harry Potter", "J.K. Rowling", 1997, 223);
+    }
+}
